ff5e9fcd3516f2f6f0edd7bc2ccae653e92f29ee
Reinaldo Silva <juninn12346@gmail.com>
2020-05-05 17:53:57 -0300

    Alterando erro na NavBar

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                link.style.animation = `navLinkFade`;
             }
         });
         //animação do icon
@@ -46,7 +46,7 @@ const navSlide = () => {
                 if (link.style.animation) {
                     link.style.animation = '';
                 } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                    link.style.animation = `navLinkFade`;
                 }
             });
             //animação do icon
